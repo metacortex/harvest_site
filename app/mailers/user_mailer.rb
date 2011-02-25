4a8f33fb8aa1049c3cff8e0ebd098adec0680387
metacortex <9ruvie@gmail.com>
2011-02-25 20:02:49 +0900

    update mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class UserMailer < ActionMailer::Base
   default :from => "harvest.kor@gmail.com"
 
@@ -7,4 +8,12 @@ class UserMailer < ActionMailer::Base
 				:to => "harvest.kor@gmail.com",
 				:subject => attrs[:title])
 	end
+
+	def rsvp(attrs)
+		@attrs = attrs
+		mail(:from => attrs[:email],
+				:to => "harvest.kor@gmail.com",
+				:subject => "컨퍼런스 참가 신청: #{attrs[:name]}")
+	end
+
 end
